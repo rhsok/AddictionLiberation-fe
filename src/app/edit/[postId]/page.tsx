aa3c82033091536178274ab9934ced7abb289a2b
rhsok <rhsok823@gmail.com>
2024-10-25 14:18:03 +0900

    feat: postEidt기능 구현시작 - 브랜치 변경 post->postEdit

---
+++ src/app/edit/[postId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditPost() {
+  return (
+    <div className='h-screen'>
+      <div className='flex-grow '></div>
+    </div>
+  );
+}
+
+export default EditPost;
